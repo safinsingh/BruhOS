c7a8ef0a97d605143fc53a5eb326ad66b9942e1b
Safin Singh <safin.singh@gmail.com>
2021-01-12 20:46:47 -0800

    boot without panicking, write to vga buffer, read stivale struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 use stivale::StivaleHeader;
@@ -7,22 +7,25 @@ use stivale::StivaleHeader;
 static STACK: [u8; 4096] = [0; 4096];
 #[link_section = ".stivale2hdr"]
 #[used]
-static STIVALE_HDR: StivaleHeader = StivaleHeader::new(&STACK[0] as *const u8);
+static STIVALE_HDR: StivaleHeader = StivaleHeader::new(STACK[0] as *const u8);
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-	// this function is the entry point, since the linker looks for a function
-	// named `_start` by default
+#[no_mangle]
+pub extern "C" fn kmain(stivale_struct_ptr: usize) -> ! {
+	let stivale_struct = unsafe { stivale::load(stivale_struct_ptr) };
 	let vga_buffer = 0xb8000 as *mut u8;
 
-	for (i, &byte) in HELLO.iter().enumerate() {
+	for (i, &byte) in stivale_struct
+		.bootloader_brand()
+		.unwrap()
+		.as_bytes()
+		.iter()
+		.enumerate()
+	{
 		unsafe {
 			*vga_buffer.offset(i as isize * 2) = byte;
 			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
@@ -31,7 +34,3 @@ pub extern "C" fn _start() -> ! {
 
 	loop {}
 }
-
-// fn kmain(stivale_struct_ptr: usize) {
-//     let stivale_struct = unsafe { stivale::load(stivale_struct_ptr) };
-// }
--- x.py
+++ x.py
@@ -15,30 +15,44 @@ def vprint(message):
 
 
 def clean():
-    _("rm -f boot.img")
+    _("rm -rf build/*")
 
 
 def build():
     _('RUSTFLAGS="-C link-arg=-Ttools/linker.ld" cargo build')
     _("cp target/x86_64-bruh_os/debug/bruh_os build/kernel.elf")
+
+
+def hdd():
     _("dd if=/dev/zero of=build/bruhos.img bs=1M count=64")
-    _("mkfs.ext2 build/bruhos.img")
-    _("sudo mount build/bruhos.img iso/")
-    _("sudo cp -Rf build/* iso/")
+    _("parted -s build/bruhos.img mklabel gpt")
+    _("parted -s build/bruhos.img mkpart primary 2048s 100%")
+    _("sudo losetup -Pf --show build/bruhos.img >loopback_dev")
+    _("mkdir -p isotmp")
+    _("sudo partprobe $(cat loopback_dev)")
+    _("sudo mkfs.ext2 $(cat loopback_dev)p1")
+    _("sudo mount $(cat loopback_dev)p1 isotmp/")
+    _("sudo mount build/bruhos.img isotmp/")
+    _("sudo cp -Rf build/kernel.elf isotmp/")
+    _("sudo cp -Rf tools/limine.cfg isotmp/")
     _("sync")
-    _("sudo umount iso/")
+    _("sudo umount isotmp/")
+    _("sudo losetup -d $(cat loopback_dev)")
+    _("rm -rf isotmp/")
+    _("rm -f loopback_dev")
     _("./lib/limine/limine-install build/bruhos.img")
 
 
 def run():
     _(
-        "qemu-system-x86_64 -m 2G -net none -smp 4 -drive format=raw,file=build/bruhos.img"
+        "qemu-system-x86_64 -m 8G -net none -smp 4 -drive format=raw,file=build/bruhos.img"
     )
 
 
 def all():
     clean()
     build()
+    hdd()
     run()
 
 
@@ -49,7 +63,8 @@ USAGE: ./x.py [-v] subcommand
 
 SUBCOMMANDS:
     clean    - clean out build files
-    build    - build kernel and write to hard disk
+    build    - build kernel
+    hdd      - create and write to hard disk
     run      - emulate with qemu
     all      - clean, build, and run
     help     - display this message"""
@@ -57,7 +72,14 @@ SUBCOMMANDS:
 
 
 def main():
-    actions = {"all": all, "clean": clean, "build": build, "run": run, "help": help}
+    actions = {
+        "all": all,
+        "clean": clean,
+        "build": build,
+        "run": run,
+        "help": help,
+        "hdd": hdd,
+    }
 
     if "-v" in sys.argv:
         verbose = True
